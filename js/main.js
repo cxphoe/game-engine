8650617dc73c8ec42d410f1a4f3036837bc47ba2
Phoenine <562818991@qq.com>
2018-02-11 19:05:49 +0800

    remove useless commet

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 var __main = function () {
-    // the following code can be replaced by a procedure for 
-    // loading images from given path; to be implemented
-
     var g = Game.instance(function (game) {
         game.replaceScene(SceneTitle)
         game.run()
