ca44360af8ad53625c45f892a6bccf10064c231b
Phoenine <562818991@qq.com>
2018-02-13 18:45:27 +0800

    refine mobile interface again.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 window.onload = function() {
+    if (isMobile()) {
+        var root = document.querySelector('#root')
+        root.style.height = '700px'
+        root.style.margin = '-350px 0 0 -200px'
+    }
     var g = Game.instance(function (game) {
         game.run()
     })
