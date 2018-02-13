a5189ece530d070e3bbb641ffddd1b9cff48c494
Phoenine <562818991@qq.com>
2018-02-13 20:18:17 +0800

    prohibit scrolling, menu showing, text selecting on mobile device

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ window.onload = function() {
         root.style.margin = `-${h/2}px 0 0 -200px`
         root.style['font-size'] = '10px'
         // 禁止双指缩放
-        document.documentElement.addEventListener('touchstart', function (event) {
+        document.documentElement.addEventListener('touchstart', (event) => {
             if (event.touches.length > 1) {
                 event.preventDefault();
             }
@@ -16,14 +16,23 @@ window.onload = function() {
         
         // 禁止双击缩放
         var lastTouchEnd = 0;
-        document.documentElement.addEventListener('touchend', function (event) {
+        document.documentElement.addEventListener('touchend', (event) => {
             var now = Date.now();
             if (now - lastTouchEnd <= 300) {
                 event.preventDefault();
             }
             lastTouchEnd = now;
         }, false);
-          
+        
+        // 禁止滚动
+        document.documentElement.addEventListener('touchmove', (event) => {
+            event.preventDefault()
+        })
+
+        // 阻止长按出现菜单
+        document.documentElement.addEventListener('touchstart', (event) => {{
+            event.preventDefault()
+        }})
     }
     var g = Game.instance(function (game) {
         game.run()
