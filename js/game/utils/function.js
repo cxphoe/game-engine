9504dfe4840d7a004c49ece9d269a97e607c85a5
Phoenine <562818991@qq.com>
2018-02-13 16:56:13 +0800

    add mobile touch event (testing...)

--- js/game/utils/function.js
+++ js/game/utils/function.js
@@ -52,6 +52,17 @@ var getLocalStorage = function () {
     }
 }
 
+var isMobile = function () {
+    // 判断是否为移动端
+    const ua = navigator.userAgent;
+    const android = /Android (\d+\.\d+)/.test(ua);
+    const iphone = ua.indexOf('iPhone') > -1;
+    const ipod = ua.indexOf('iPod') > -1;
+    const ipad = ua.indexOf('iPad') > -1;
+    const nokiaN = ua.indexOf('NokiaN') > -1;
+    return android || iphone || ipod || ipad || nokiaN;
+}
+
 var nullFunc = function () {}
 
 var log = console.log.bind(console)
