359475a95d63537d7716966daf41b556ecfe53fc
Phoenine <562818991@qq.com>
2018-02-13 18:57:23 +0800

    limit scale on mobile device

--- js/main.js
+++ js/main.js
@@ -3,6 +3,24 @@ window.onload = function() {
         var root = document.querySelector('#root')
         root.style.height = '700px'
         root.style.margin = '-350px 0 0 -200px'
+        
+        // 禁止双指缩放
+        document.documentElement.addEventListener('touchstart', function (event) {
+            if (event.touches.length > 1) {
+                event.preventDefault();
+            }
+        }, false);
+        
+        // 禁止双击缩放
+        var lastTouchEnd = 0;
+        document.documentElement.addEventListener('touchend', function (event) {
+            var now = Date.now();
+            if (now - lastTouchEnd <= 300) {
+                event.preventDefault();
+            }
+            lastTouchEnd = now;
+        }, false);
+          
     }
     var g = Game.instance(function (game) {
         game.run()
